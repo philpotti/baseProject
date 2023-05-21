d7f53e08b94eab91a5932f2a2a9517d7527885d8
philpotti <89038510+philpotti@users.noreply.github.com>
2023-05-21 18:10:44 -0300

    ¡ cy.origin DONE !

--- cypress/integration/examples/Test12-HandlingCHILDWindows.js
+++ cypress/integration/examples/Test12-HandlingCHILDWindows.js
@@ -2,12 +2,17 @@
 
 describe('2nd Test Suite', function () {
   it('My second test case', function () {
-    cy.visit('https://app-95fireplaster-stg.azurewebsites.net/login');
-    cy.get('button.buttonLogin').click();
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+    cy.get('#openwindow').invoke('removeAttr', 'onclick').click();
 
-    cy.origin('https://launchpad.classlink.com/', () => {
-      cy.get('.label-text').click();
+    cy.visit('http://www.qaclickacademy.com/');
+    cy.origin('http://www.qaclickacademy.com/', () => {
+      cy.get('ul.navbar-nav a[href*="contactus"]').click();
+      cy.get('div h2').should('contain', 'Contact')
     });
+
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+
   });
 });
-// just a comments
+
