a45d0f8781e4da09eb190328bc860cbf5686dec7
philpotti <89038510+philpotti@users.noreply.github.com>
2023-05-22 19:15:10 -0300

    Commiting iFrame Fix with Wait(3000)

--- cypress/integration/examples/Test13-iFrames.js
+++ cypress/integration/examples/Test13-iFrames.js
@@ -4,12 +4,19 @@ import 'cypress-iframe';
 
 describe('13th Test Suite', function () {
   it('My 13th test case', function () {
-    cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
 
-    cy.frameLoaded('#courses-iframe');
-    cy.iframe().find('li a[href*="mentorship"]').eq(0).click();
+    cy.frameLoaded("#courses-iframe")
+    cy.iframe().find("a[href*='mentorship']").eq(0).click()
 
-    cy.iframe().find('h1[class*="pricing-title"]').should('have.length', 2);
-  });
-});
+    cy.wait(3000);
 
+    cy.iframe().find("h1[class*='pricing-title']").should("have.length", 2)
+    cy.get("input[id='name']").type("test")
+    cy.get("input[value*='radio']").eq(1).click()
+
+    cy.frameLoaded("#courses-iframe")
+    cy.iframe().find("a[href*='mentorship']").eq(0).click()
+    cy.iframe().find("h1[class*='pricing-title']").should("have.length", 2)
+  })
+})
