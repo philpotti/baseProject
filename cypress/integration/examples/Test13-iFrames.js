1b869cda7bca51026d881bbdbf7c2460929c997b
philpotti <89038510+philpotti@users.noreply.github.com>
2023-05-21 21:33:20 -0300

    Push prueba con Erick

--- cypress/integration/examples/Test13-iFrames.js
+++ cypress/integration/examples/Test13-iFrames.js
@@ -6,6 +6,10 @@ describe('13th Test Suite', function () {
   it('My 13th test case', function () {
     cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
 
+    cy.frameLoaded('#courses-iframe');
+    cy.iframe().find('li a[href*="mentorship"]').eq(0).click();
+
+    cy.iframe().find('h1[class*="pricing-title"]').should('have.length', 2);
   });
 });
 
